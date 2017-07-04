14202b126af8a90046be29d755dafe52eda963d0
Layne Armstrong <layne.armstrong@gmail.com>
2017-07-04 00:56:31 +0000

    Sessions

--- app/actions.rb
+++ app/actions.rb
@@ -1,58 +1,54 @@
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-      "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
     end
 end
+get '/' do
+    @posts = Post.order(created_at: :desc)
+    erb(:index)
+end
 
+get'/signup' do 
+    @user = User.new
+    erb(:signup)
+end
 
-get '/' do 
-  # your previous post code is up here
-  # add the following @posts line
+get '/login' do
+    erb(:login)
+end
 
-    @post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http:naserca.com/images/shark.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "sharky_j",
-            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-        }]
-    }
+get "/logout" do
+    session[:user_id] = nil
+    redirect to ('/')
+end
 
-    @post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk_whalum",
-            text: "#weekendvibes"
-        }]
-    }
+post '/login' do
+    username = params[:username]
+    password = params[:password]
     
-    @post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin_peppa",
-            text: "lunchtime! ;)"
-        }]
-    }
+    user = User.find_by(username: username)
     
-    [@post_shark, @post_whale, @post_marlin.to_s]
+    if user && user.password == password
+        session[:user_id] = user.id
+            redirect to('/')
+        else
+            @error_message = "Login failed."
+            erb(:login)
+            
+    end
+end 
 
-  @posts = [@post_shark, @post_whale, @post_marlin]
-  
-  erb(:index)
+post '/signup' do
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    if @user.save
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
 end
